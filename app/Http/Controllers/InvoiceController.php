40c936608b67dc734745bf15b23b3d6a60361164
Subhasish Samaddar <subhassahaniflancer@gmail.com>
2022-05-06 22:45:30 +0530

    updfate invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -43,7 +43,7 @@ class InvoiceController extends Controller
         $customer_email = $request->customer_email;
 
         $query = DB::table('invoices');
-        if($reference_id!=''){
+        if($invoice_id!=''){
             $query->where('invoice_id',$invoice_id);
         }if($receipt!=''){
             $query->where('receipt',$receipt);
@@ -54,39 +54,42 @@ class InvoiceController extends Controller
         }
         $result = $query->get();
         //print_r($result);exit;
-        $all_links = $result;
+        $all_invoices = $result;
 
 
 
-        $api_key = session('merchant_key');
-        $api_secret = session('merchant_secret');
-
-
-        $api = new Api($api_key, $api_secret);
+        //$api_key = session('merchant_key');
+        //$api_secret = session('merchant_secret');
+        //$api = new Api($api_key, $api_secret);
         //$api = new Api('rzp_test_YRAqXZOYgy9uyf', 'uSaaMQw3jHK0MPtOnXCSSg51');
-        $all_invoices = $api->invoice->all();
+        //$all_invoices = $api->invoice->all();
 
         $html = '';
-        if(!empty($all_invoices->items)){
-            foreach($all_invoices->items as $invoice){
-                if($invoice_id==$invoice->id || $receipt==$invoice->receipt || $customer_contact==$invoice->customer_details->contact || $customer_email==$invoice->customer_details->email){
-                    $html.='<tr>
-                        <th scope="row">'.$invoice->id.'</th>
-                        <td>'.number_format(($invoice->line_items[0]->net_amount)/100,2).'</td>
-                        <td>'.$invoice->receipt.'</td>
-                        <td>'.date('Y-m-d',$invoice->created_at).'</td>
-                        <td>'.$invoice->customer_details->name.' ('.$invoice->customer_details->contact.'/ '.$invoice->customer_details->email.')</td>
-                        <td>'.$invoice->short_url.'</td>
-                        <td>';
-                            if($invoice->status=='cancelled'){
-                                $html.='<span class="new badge red">'.$invoice->status.'</span>';
-                            }
-                            else{
-                                $html.='<span class="new badge blue">'.$invoice->status.'</span>';
-                            }
-                            $html.='</td>
-                    </tr>';
-                }
+        if(!empty($all_invoices)){
+            foreach($all_invoices as $invoice){
+                $customer_details = Helper::get_customer_details($invoice->customer_id);  
+                $items = explode(',',$invoice->item_id);
+                $amount = 0;
+                foreach($items as $iid){
+                    $item_details = Helper::get_item_details($iid);
+                    $amount+=$item_details->amount;
+                } 
+                $html.='<tr>
+                    <th scope="row">'.$invoice->id.'</th>
+                    <td>'.number_format($amount,2).'</td>
+                    <td>'.$invoice->receipt.'</td>
+                    <td>'.$invoice->created_at.'</td>
+                    <td>'.$customer_details->name.' ('.$customer_details->contact.'/ '.$customer_details->email.')</td>
+                    <td>'.$invoice->short_url.'</td>
+                    <td>';
+                        if($invoice->status=='cancelled'){
+                            $html.='<span class="new badge red">'.$invoice->status.'</span>';
+                        }
+                        else{
+                            $html.='<span class="new badge blue">'.$invoice->status.'</span>';
+                        }
+                        $html.='</td>
+                </tr>';
             }
         }
         return response()->json(array('html'=>$html));
