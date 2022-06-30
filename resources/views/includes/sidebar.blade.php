b8c7ae987c4e6a3c510073d91eccc9b2f5512670
Subhasish Samaddar <subhassahaniflancer@gmail.com>
2022-06-30 12:57:30 +0530

    sidebar navigation modification

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -46,34 +46,35 @@
             <li class="nav-item">
                 <a href="{{ route('settlements')}}" class="nav-link {{ in_array(Request::segment(1),array('settlements')) ? 'active' : '' }}"> <i class="nav-icon far fa-plus-square"></i> <p>Settlements</p> </a>
             </li>
-            <li class="nav-item">
-                <a href="{{ route('payment-links')}}" class="nav-link {{ in_array(Request::segment(1),array('payment-links')) ? 'active' : '' }}"> <i class="nav-icon far fa-plus-square"></i> <p>Payment Links</p> </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('payment-pages')}}" class="nav-link {{ in_array(Request::segment(1),array('payment-pages')) ? 'active' : '' }}"> <i class="nav-icon far fa-plus-square"></i> <p>Payment Pages</p> </a>
-            </li>
-            <li class="nav-header"></li>
+            
+
             <li class="nav-item {{ in_array(Request::segment(1),array('invoices','items')) ? 'menu-open' : '' }}">
-                <a href="#" class="nav-link {{ in_array(Request::segment(2),array('invoices','items')) ? 'active' : '' }}"> <i class="nav-icon fas fa-users"></i> <p>Invoices <i class="right fas fa-angle-left"></i></p> </a>
+                <a href="#" class="nav-link {{ in_array(Request::segment(2),array('invoices','items')) ? 'active' : '' }}"> <i class="nav-icon fas fa-list"></i> <p>Invoices <i class="right fas fa-angle-left"></i></p> </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item"><a href="{{ route('invoices')}}" class="nav-link {{ Request::segment(1) === 'invoices' ? 'active' : null }}"><i class="nav-icon far fa-circle text-success"></i><p>Invoices</p> </a> </li>
                     <li class="nav-item"><a href="{{ route('items')}}" class="nav-link {{ Request::segment(1) === 'items' ? 'active' : null }}"><i class="nav-icon far fa-circle text-warning"></i><p>Items</p> </a></li>
                 </ul>
             </li>
 
-            <li class="nav-item">
-                <a href="{{ route('customer.index') }}" class="nav-link {{ in_array(Request::segment(1),array('customer')) ? 'active' : '' }}"> <i class="nav-icon fas fa-tachometer-alt"></i> <p>Customers</p> </a>
-            </li>
-            <li class="nav-item {{ in_array(Request::segment(1),array('pages')) ? 'menu-open' : '' }}">
-                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('settings','countries','states')) ? 'active' : '' }}"> <i class="nav-icon fas fa-tachometer-alt"></i> <p>Settings <i class="right fas fa-angle-left"></i></p> </a>
-                <ul class="nav nav-treeview">
-                    <li class="nav-item"><a href="#" class="nav-link {{ Request::segment(1) === 'payment-templates' ? 'active' : null }}"><i class="nav-icon far fa-circle text-success"></i><p>Payment Templates</p> </a> </li>
 
+            <li class="nav-item {{ in_array(Request::segment(1),array('payment-links','payment-pages')) ? 'menu-open' : '' }}">
+                <a href="#" class="nav-link {{ in_array(Request::segment(2),array('payment-links','payment-pages')) ? 'active' : '' }}"> <i class="nav-icon fas fa-dollar"></i> <p>Payment Option<i class="right fas fa-angle-left"></i></p> </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{ route('payment-links')}}" class="nav-link {{ in_array(Request::segment(1),array('payment-links')) ? 'active' : '' }}"> <i class="nav-icon far fa-circle text-success"></i> <p>Payment Links</p> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('payment-pages')}}" class="nav-link {{ in_array(Request::segment(1),array('payment-pages')) ? 'active' : '' }}"> <i class="nav-icon far fa-circle text-info"></i><p>Payment Pages</p> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"> <i class="nav-icon far fa-circle text-warning"></i> <p>Payment Button</p> </a>
+                    </li>
                 </ul>
             </li>
 
+
             <li class="nav-item">
-                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('reports')) ? 'active' : '' }}"> <i class="nav-icon fas fa-tachometer-alt"></i> <p>Reports <i class="right fas fa-angle-left"></i></p> </a>
+                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('reports')) ? 'active' : '' }}"> <i class="nav-icon fas fa-file"></i> <p>Reports <i class="right fas fa-angle-left"></i></p> </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-success"></i><p>Report A</p> </a> </li>
                     <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-warning"></i><p>Report B</p> </a> </li>
@@ -82,6 +83,41 @@
                     <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-danger"></i><p>Report E</p> </a> </li>
                 </ul>
             </li>
+
+
+            <li class="nav-item">
+                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('reports')) ? 'active' : '' }}"> <i class="nav-icon fas fa-tachometer-alt"></i> <p>Payouts <i class="right fas fa-angle-left"></i></p> </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-success"></i><p>Payout Balance</p> </a> </li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-warning"></i><p>Add beneficiary</p> </a> </li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-info"></i><p>Payout Transaction Report</p> </a> </li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-primary"></i><p>Single Payoyut Transfer</p> </a> </li>
+                </ul>
+            </li>
+
+
+            <li class="nav-item">
+                <a href="{{ route('customer.index') }}" class="nav-link {{ in_array(Request::segment(1),array('customer')) ? 'active' : '' }}"> <i class="nav-icon fas fa-users"></i> <p>Customers</p> </a>
+            </li>
+
+
+            <li class="nav-item {{ in_array(Request::segment(1),array('pages')) ? 'menu-open' : '' }}">
+                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('settings','countries','states')) ? 'active' : '' }}"> <i class="nav-icon fas fa-gear"></i> <p>Settings <i class="right fas fa-angle-left"></i></p> </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item"><a href="#" class="nav-link {{ Request::segment(1) === 'payment-templates' ? 'active' : null }}"><i class="nav-icon far fa-circle text-success"></i><p>Payment Templates</p> </a> </li>
+
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link {{ in_array(Request::segment(1),array('reports')) ? 'active' : '' }}"> <i class="nav-icon fas fa-flag"></i> <p>Chargeback <i class="right fas fa-angle-left"></i></p> </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-success"></i><p>Open</p> </a> </li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon far fa-circle text-warning"></i><p>Close</p> </a> </li>
+                </ul>
+            </li>
+
+            
             <li class="nav-header">Account</li>
             <li class="nav-item has-treeview {{ in_array(Request::segment(1),array('profile_update','change-password')) ? 'menu-open' : null }}">
                 <a href="#" class="nav-link">
